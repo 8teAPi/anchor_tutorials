5d228df82f18d20517f497f8684acc26ec1d890d
8teAPi <8teapi@gmail.com>
2021-12-12 08:37:32 +0000

    refactored base1 test

--- base1/tests/base1.js
+++ base1/tests/base1.js
@@ -9,16 +9,15 @@ describe("basic-1", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(provider);
 
-  it("Creates and initializes an account in a single atomic transaction (simplified)", async () => {
-    // #region code-simplified
-    // The program to execute.
-    const program = anchor.workspace.Basic1;
+  // The program to execute.
+  const program = anchor.workspace.Basic1;
 
-    // The Account to create.
-    const myAccount = anchor.web3.Keypair.generate();
+  // The Account to create.
+  const myAccount = anchor.web3.Keypair.generate();
+
+  it("Creates and initializes an account in a single atomic transaction", async () => {
 
     // Create the new account and initialize it with the program.
-    // #region code-simplified
     await program.rpc.initialize(new anchor.BN(1234), {
       accounts: {
         myAccount: myAccount.publicKey,
@@ -27,7 +26,6 @@ describe("basic-1", () => {
       },
       signers: [myAccount],
     });
-    // #endregion code-simplified
 
     // Fetch the newly created account from the cluster.
     const account = await program.account.myAccount.fetch(myAccount.publicKey);
@@ -35,17 +33,10 @@ describe("basic-1", () => {
     // Check it's state was initialized.
     assert.ok(account.data.eq(new anchor.BN(1234)));
 
-    // Store the account for the next test.
-    _myAccount = myAccount;
   });
 
+
   it("Updates a previously created account", async () => {
-    const myAccount = _myAccount;
-
-    // #region update-test
-
-    // The program to execute.
-    const program = anchor.workspace.Basic1;
 
     // Invoke the update rpc.
     await program.rpc.update(new anchor.BN(4321), {
@@ -60,23 +51,5 @@ describe("basic-1", () => {
     // Check it's state was mutated.
     assert.ok(account.data.eq(new anchor.BN(4321)));
 
-    // #endregion update-test
   });
 });
-
-
-
-// const anchor = require('@project-serum/anchor');
-
-// describe('base1', () => {
-
-//   // Configure the client to use the local cluster.
-//   anchor.setProvider(anchor.Provider.env());
-
-//   it('Is initialized!', async () => {
-//     // Add your test here.
-//     const program = anchor.workspace.Base1;
-//     const tx = await program.rpc.initialize();
-//     console.log("Your transaction signature", tx);
-//   });
-// });
