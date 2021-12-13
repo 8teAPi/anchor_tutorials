ce85d1c15f1fd571494ead4aaff2bc6cf7229469
8teAPi <8teapi@gmail.com>
2021-12-13 21:14:30 +0700

    base2

--- base2/programs/base2/src/lib.rs
+++ base2/programs/base2/src/lib.rs
@@ -1,14 +1,63 @@
 use anchor_lang::prelude::*;
 
+/*
+ * Here we have a simple **Counter** program, where anyone can 
+ * create a counter, but only the assigned **authority** can
+ * increment it.
+ */
+
+
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
 pub mod base2 {
     use super::*;
-    pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
+    pub fn create(ctx: Context<Create>, authority: Pubkey) -> ProgramResult {
+        let counter = &mut ctx.accounts.counter;
+        counter.authority = authority;
+        counter.count = 0;
         Ok(())
     }
+
+    pub fn increment(ctx: Context<Increment>) -> ProgramResult {
+        let counter = &mut ctx.accounts.counter;
+        counter.count += 1;
+        Ok(())
+    }
+    
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Create<'info>{
+    // why 40?
+    #[account(init, payer = user, space = 8 + 40)]
+    pub counter: Account<'info, Counter>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program<'info, System>, 
+}
+
+#[derive(Accounts)]
+pub struct Increment<'info>{
+    /*
+     * has_one: enforces the constraint that 
+     * Increment.counter.authority == Increment.authority.key
+     * 
+     * Signer: type : anchor_lang : This enforces the constraint
+     * that the authority account signed the transaction. However,
+     * anchor doesn't fetch the data on that account.
+     */
+    #[account(mut, has_one = authority)]
+    pub counter: Account<'info, Counter>,
+    pub authority: Signer<'info>,
+}
+
+#[account]
+pub struct Counter {
+    /*
+     * authority : field : here : field is of type `Pubkey`, which is
+     * a solana_program type 
+     */
+    pub authority: Pubkey,
+    pub count: u64,
+}
--- base2/tests/base2.js
+++ base2/tests/base2.js
@@ -1,14 +1,61 @@
-const anchor = require('@project-serum/anchor');
+const assert = require("assert");
+const anchor = require("@project-serum/anchor");
+const { SystemProgram } = anchor.web3;
 
-describe('base2', () => {
+describe("basic-2", () => {
+  // Use a local provider.
+  const provider = anchor.Provider.local();
 
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.Provider.env());
+  anchor.setProvider(provider);
 
-  it('Is initialized!', async () => {
-    // Add your test here.
-    const program = anchor.workspace.Base2;
-    const tx = await program.rpc.initialize();
-    console.log("Your transaction signature", tx);
+  // Program for the tests.
+  const program = anchor.workspace.Basic2;
+
+  // The Account to create.
+  const counterAcctKeypair = anchor.web3.Keypair.generate();
+
+  it("Creates a counter", async () => {
+
+    await program.rpc.create(provider.wallet.publicKey, {
+      accounts :{
+        counter: counterAcctKeypair.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers:[counterAcctKeypair],
+    });
+
+
+    // Fetch the newly created account from the cluster.
+    const counterAccount = await program.account.counter.fetch(counterAcctKeypair.publicKey);
+
+
+    // Check the authority and count
+    assert.ok(counterAccount.authority.equals(provider.wallet.publicKey));  
+    assert.ok(counterAccount.count.toNumber() === 0);
+
+  });
+
+
+  it("Updates a counter", async () => {
+
+    // Invoke the increment rpc.
+    await program.rpc.increment({
+      accounts: {
+        counter: counterAcctKeypair.publicKey,
+        authority: provider.wallet.publicKey,
+      },
+    });
+
+    // Fetch the newly updated account.
+    const counterAccount = await program.account.counter.fetch(counterAcctKeypair.publicKey);
+
+    // Check it's state was mutated.
+    // Check the authority and count
+    assert.ok(counterAccount.authority.equals(provider.wallet.publicKey));  
+    assert.ok(counterAccount.count.toNumber() === 1);
+    assert.ok(counterAccount.count.eq(new anchor.BN(1)));
+
   });
 });
